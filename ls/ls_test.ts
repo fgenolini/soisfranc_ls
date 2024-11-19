60e5b974ae2f91197ebdd37b7f025aeb9409c055
François Génolini <fgenolini@entegral.com>
2024-11-19 23:25:11 +0000

    allow unit test to run from git root or ls folder

--- ls/ls_test.ts
+++ ls/ls_test.ts
@@ -1,8 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
 import { tree } from "./mod.ts";
+import { exists } from "jsr:@std/fs/exists";
 
 Deno.test("dir with one file test", async () => {
-  // Expect the test to be run from the root of the git repo, not the "ls" directory
-  const actual = await tree("ls/one_file/");
-  assertEquals(actual, ["","one_file.txt"]);
+  const e = await exists("./one_file", { isDirectory: true });
+  if (e) {
+    // Run from the "ls" directory, not the root of the git repo
+    const actual = await tree("./one_file/");
+    assertEquals(actual, ["", "one_file.txt"]);
+  } else {
+    // Run from the root of the git repo, not the "ls" directory
+    const actual = await tree("ls/one_file/");
+    assertEquals(actual, ["", "one_file.txt"]);
+  }
 });

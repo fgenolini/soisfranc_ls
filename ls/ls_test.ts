5d30b4fa7c3630406e9eb940c3f47bf3c43b8dea
François Génolini <fgenolini@entegral.com>
2024-11-19 21:36:21 +0000

    fix test for git action

--- ls/ls_test.ts
+++ ls/ls_test.ts
@@ -2,6 +2,7 @@ import { assertEquals } from "jsr:@std/assert";
 import { tree } from "./mod.ts";
 
 Deno.test("dir with one file test", async () => {
-  const actual = await tree("one_file/");
+  // Expect the test to be run from the root of the git repo, not the ls directory
+  const actual = await tree("ls/one_file/");
   assertEquals(actual, ["","one_file.txt"]);
 });
